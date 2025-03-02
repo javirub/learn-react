35c7ba561773304220ca0953c063cb1a46c26428
Javirub <jrubio@laberit.com>
2025-03-02 12:08:14 +0100

    mvp tictactoe

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,35 +1,108 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {useState} from "react";
+
+const TURNS = {
+    X: 'x',
+    O: 'o'
+}
+
+const WINNERS = {
+    X: 'x',
+    O: 'o',
+    TIE: 'Tie'
+}
+
+const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+const Square = ({children, isSelected, updateBoard, index}) => {
+    const className = `square ${isSelected ? 'is-selected' : ''}`
+
+    const handleClick = () => {
+        updateBoard(index)
+    }
+    return (
+        <div onClick={handleClick} className={className}>
+            {children}
+        </div>
+    )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [board, setBoard] = useState(
+        Array(9).fill(null)
+    )
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [turn, setTurn] = useState(TURNS.X)
+    const [winner, setWinner] = useState(null)
+
+    const checkWinner = (boardToCheck) => {
+        for (const combination of WINNING_COMBINATIONS) {
+            const [a, b, c] = combination
+            if
+                (boardToCheck[a] &&
+                boardToCheck[a] === boardToCheck[b] &&
+                boardToCheck[a] === boardToCheck[c]) {
+                return boardToCheck[a] === TURNS.X ? WINNERS.X : WINNERS.O;
+                }
+        }
+        return null
+    }
+
+    const updateBoardAndChangeTurn = (index) => {
+        if (board[index] || winner) return
+
+        const newBoard = [...board]
+        newBoard[index] = turn
+        setBoard(newBoard)
+
+        const newWinner = checkWinner(newBoard)
+        if (newWinner) {
+            setWinner(newWinner)
+        } else if (newBoard.every(square => square !== null)) {
+            setWinner(WINNERS.TIE)
+        }
+
+        const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+        setTurn(newTurn)
+    }
+
+    return (
+        <main className='board'>
+            <h1>Tic Tac Toe</h1>
+            <section className="game">
+                {
+                    board.map((_, index) => {
+                        return (
+                            <Square
+                                key={index}
+                                index={index}
+                                updateBoard={updateBoardAndChangeTurn}
+                            >
+                                {board[index]}
+                            </Square>
+                        )
+                    })
+                }
+            </section>
+            <section className="turn">
+                <Square isSelected={turn === TURNS.X}>
+                    {TURNS.X}
+                </Square>
+                <Square isSelected={turn === TURNS.O}>
+                    {TURNS.O}
+                </Square>
+            </section>
+        </main>
+    )
 }
 
 export default App
